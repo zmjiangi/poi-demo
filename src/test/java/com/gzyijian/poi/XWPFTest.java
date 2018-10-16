c0dd217efaa557a9eea4748babdcaf59f1152e39
zmjiangi <zmjiangi@qq.com>
2018-10-16 15:13:17 +0800

    初始化项目

---
+++ src/test/java/com/gzyijian/poi/XWPFTest.java
@@ -0,0 +1,126 @@
+package com.gzyijian.poi;
+
+import org.apache.poi.xwpf.usermodel.*;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class XWPFTest {
+
+    /**
+     * 用一个docx文档作为模板，然后替换其中的内容，再写入目标文档中。
+     */
+    @Test
+    public void out() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("data.txt"))
+        );
+        String lineContent;
+        while ((lineContent = bufferedReader.readLine()) != null) {
+            String[] lineContentArray = lineContent.split("\t");
+            Map<String, Object> params = new HashMap<>();
+            params.put("序号", lineContentArray[0]);
+            params.put("名称", lineContentArray[1]);
+            try (
+                    InputStream templateInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("template.docx");
+                    OutputStream outputStream = new FileOutputStream(Object.class.getResource("/").getPath() + lineContentArray[1] + ".docx")
+            ) {
+                XWPFDocument doc = new XWPFDocument(templateInputStream);
+                // 替换段落里面的变量
+                this.replaceInPara(doc, params);
+                // 替换表格里面的变量
+                this.replaceInTable(doc, params);
+                doc.write(outputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * 替换段落里面的变量
+     *
+     * @param doc    要替换的文档
+     * @param params 参数
+     */
+    private void replaceInPara(XWPFDocument doc, Map<String, Object> params) {
+        Iterator<XWPFParagraph> iterator = doc.getParagraphsIterator();
+        XWPFParagraph para;
+        while (iterator.hasNext()) {
+            para = iterator.next();
+            this.replaceInPara(para, params);
+        }
+    }
+
+    /**
+     * 替换段落里面的变量
+     *
+     * @param para   要替换的段落
+     * @param params 参数
+     */
+    private void replaceInPara(XWPFParagraph para, Map<String, Object> params) {
+        List<XWPFRun> runs;
+        Matcher matcher;
+        if (this.matcher(para.getParagraphText()).find()) {
+            runs = para.getRuns();
+            for (int i = 0; i < runs.size(); i++) {
+                XWPFRun run = runs.get(i);
+                String runText = run.toString();
+                matcher = this.matcher(runText);
+                if (matcher.find()) {
+                    while ((matcher = this.matcher(runText)).find()) {
+                        runText = matcher.replaceFirst(String.valueOf(params.get(matcher.group(1))));
+                    }
+                    // 直接调用XWPFRun的setText()方法设置文本时，在底层会重新创建一个XWPFRun，把文本附加在当前文本后面，
+                    // 所以我们不能直接设值，需要先删除当前run,然后再自己手动插入一个新的run。
+                    para.removeRun(i);
+                    para.insertNewRun(i).setText(runText);
+                }
+            }
+        }
+    }
+
+    /**
+     * 替换表格里面的变量
+     *
+     * @param doc    要替换的文档
+     * @param params 参数
+     */
+    private void replaceInTable(XWPFDocument doc, Map<String, Object> params) {
+        Iterator<XWPFTable> iterator = doc.getTablesIterator();
+        XWPFTable table;
+        List<XWPFTableRow> rows;
+        List<XWPFTableCell> cells;
+        List<XWPFParagraph> paras;
+        while (iterator.hasNext()) {
+            table = iterator.next();
+            rows = table.getRows();
+            for (XWPFTableRow row : rows) {
+                cells = row.getTableCells();
+                for (XWPFTableCell cell : cells) {
+                    paras = cell.getParagraphs();
+                    for (XWPFParagraph para : paras) {
+                        this.replaceInPara(para, params);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 正则匹配字符串
+     */
+    private Matcher matcher(String str) {
+        Pattern pattern = Pattern.compile("\\$\\{(.+?)\\}", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+        return matcher;
+    }
+
+}
